d4246503d1b24d9752bdbfd92a6ddb0feea70d0a
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-02-14 00:22:21 -0800

    created player and gun animations for map

---
+++ scenes/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $Camera
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 7.0
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	if event.is_action_pressed("shoot"):
+		play_shoot_effects()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if animation_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+	
+	move_and_slide()
+
+func play_shoot_effects():
+	animation_player.stop()
+	animation_player.play("shoot")

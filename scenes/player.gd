a094c6e1318fc18044085e0ddb7c495d30b59c12
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-02-14 22:34:38 -0800

    Revert "Merge branch 'dev' into gun"
    This reverts commit a635178a2fa7691ad090b3de3eb938b8ab825d89, reversing
    changes made to 7d6bf7b653deaf062f7c3ef79310bd427b3095c4.

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,19 +1,10 @@
-class_name Player extends CharacterBody3D
+extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var interact_cast: RayCast3D = $Camera/InteractCast
-@onready var speed_boost_timer: Timer = $SpeedBoostTimer
 
-var items: Array[Item] = []
-var selected_item_slot = 0
-var is_speed_boosted: bool = false
-
-const MAX_ITEM_COUNT: int = 3
-const DEFAULT_SPEED: float = 10.0
-const BOOSTED_SPEED: float = 16.0
-const DEFAULT_JUMP_VELOCITY: float = 7.0
-const BOOSTED_JUMP_VELOCITY: float = 12.0
+const SPEED = 10.0
+const JUMP_VELOCITY = 7.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -25,42 +16,26 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera.rotate_x(-event.relative.y * 0.005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 	
-	if event.is_action_pressed("shoot"):
-		play_shoot_effects()
-	
-	if event.is_action_pressed("use_item") and items.size() > 0:
-		call(items[selected_item_slot].player_function_name)
-	
-	if event.is_action_pressed("jump") and is_on_floor():
-		velocity.y = DEFAULT_JUMP_VELOCITY
-	
-	if items.size() > 0:
-		if event.is_action_pressed("item_slot_right"):
-			selected_item_slot = (selected_item_slot + 1) % items.size()
-		if event.is_action_pressed("item_slot_left"):
-			selected_item_slot = (selected_item_slot + items.size()-1) % items.size()
 
 func _physics_process(delta: float) -> void:
-	print(selected_item_slot)
-	if interact_cast.is_colliding():
-		#change this block of code later so it works well with all kinds of interactables
-		var target = interact_cast.get_collider()
-		if target is LootBox:
-			if Input.is_action_just_pressed("interact") and items.size() < MAX_ITEM_COUNT:
-				items.append(target.obtain_item())
-	
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var speed = BOOSTED_SPEED if is_speed_boosted else DEFAULT_SPEED
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	if animation_player.current_animation == "shoot":
 		pass
@@ -71,14 +46,3 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("idle")
 	
 	move_and_slide()
-
-func play_shoot_effects():
-	animation_player.stop()
-	animation_player.play("shoot")
-
-func boost_speed():
-	speed_boost_timer.start()
-	is_speed_boosted = true
-
-func _on_speed_boost_timer_timeout() -> void:
-	is_speed_boosted = false

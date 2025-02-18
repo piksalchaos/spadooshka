fbc9189b2cc15f460b25f34a2356ffea9ecfd0de
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-02-17 23:13:38 -0800

    some extra changes for last merge

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -16,13 +16,21 @@ func _ready() -> void:
 	camera_collider.target_position = DEFAULT_CAMERA_POSITION
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _unhandled_input(event: InputEvent) -> void:	
-	if event is InputEventMouseMotion:
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
+	if event.is_action_pressed("escape"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		player.rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		head.rotation_degrees.x = clamp(head.rotation_degrees.x, -60, 60)
 
 func _process(delta: float) -> void:
+	if not is_multiplayer_authority(): return
 	if camera_collider.is_colliding():
 		global_transform.origin = camera_collider.get_collision_point()
 	else:
--- scenes/test_world_multiplayer.gd
+++ scenes/test_world_multiplayer.gd
@@ -7,16 +7,6 @@ var enet_peer = ENetMultiplayerPeer.new()
 @onready var item_list_label: Label = $CanvasLayer/HUD/ItemListLabel
 @onready var main_menu: PanelContainer = $CanvasLayer/MainMenu
 
-#this code below is stupid.
-#better to save items not on the player node, but to something else parenting it!
-#func _process(delta: float) -> void:
-	#item_list_label.text = ""
-	#for i in player.items.size():
-		#if i == player.selected_item_slot:
-			#item_list_label.text += "speed boost <-\n" #in the time of writing this code, only speed boost items exist. bc the game is no text, these will be replaced with icons instead later
-		#else:
-			#item_list_label.text += "speed boost\n"
-
 func _on_main_menu_host_button_pressed() -> void:
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer

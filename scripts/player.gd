3e1e091938993ef563e90b7b1cb91135616547b1
daylin <tootymcfruity8@gmail.com>
2025-02-17 20:29:32 -0800

    better movement
    - fixed movement
    - organized timers under one node

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,9 @@ class_name Player extends CharacterBody3D
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera
 @onready var interact_cast: RayCast3D = $Head/Camera/InteractCast
-@onready var speed_boost_timer: Timer = $SpeedBoostTimer
-@onready var dash_timer: Timer = $DashTimer
-@onready var dash_cooldown_timer: Timer = $DashCooldownTimer
+@onready var speed_boost_timer: Timer = $Timers/SpeedBoostTimer
+@onready var dash_timer: Timer = $Timers/DashTimer
+@onready var dash_cooldown_timer: Timer = $Timers/DashCooldownTimer
 @export var equipped_gun: Gun
 
 const MAX_ITEM_COUNT: int = 3
@@ -13,13 +13,15 @@ const DEFAULT_SPEED: float = 10.0
 const BOOSTED_SPEED: float = 100.0
 const DASH_SPEED: float = 50.0
 
-const DEFAULT_JUMP_VELOCITY: float = 7.0
+const DEFAULT_JUMP_VELOCITY: float = 8.0
 const BOOSTED_JUMP_VELOCITY: float = 12.0
-const WALL_JUMP_VELOCITY: float = 30.0
+const WALL_JUMP_VELOCITY: float = 20.0
 const WALL_JUMP_Y_DIRECTION: float = 0.2
+const WALL_SLIDE_GRAVITY: float = -9.0
 
-const ACCELERATION: float = 5.0
-const FRICTION: float = 15.0
+const ACCELERATION: float = 2.0
+const IN_AIR_DECELERATION: float = 0.5
+const FRICTION: float = 10.0
 
 var items: Array[Item] = []
 var selected_item_slot = 0
@@ -49,24 +51,27 @@ func _physics_process(delta: float) -> void:
 			if Input.is_action_just_pressed("interact") and items.size() < MAX_ITEM_COUNT:
 				items.append(target.obtain_item())
 	
-	is_wall_sliding = is_on_wall_only()
-	
-	if is_wall_sliding:
-		velocity.y += -3 * delta
-	elif not is_on_floor():
+	if not is_on_wall_only(): # if not on wall, dont wall slide & have regular gravity
+		is_wall_sliding = false
 		velocity += get_gravity() * delta
+	elif not is_wall_sliding and is_on_wall_only(): # if just got on wall, cancel y velo & enable sliding
+		is_wall_sliding = true
+		velocity.y = 0
+	elif is_wall_sliding and velocity.y <= 0: # if sliding + falling back down, have less gravity
+		velocity.y += WALL_SLIDE_GRAVITY * delta
 	
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var speed = BOOSTED_SPEED if is_speed_boosted else DEFAULT_SPEED
+	var max_speed = BOOSTED_SPEED if is_speed_boosted else DEFAULT_SPEED
+	var deceleration = FRICTION if is_on_floor() else IN_AIR_DECELERATION
 	
 	if not is_dashing:
 		if direction:
-			velocity.x = lerp(velocity.x, direction.x * speed, ACCELERATION * delta)
-			velocity.z =  lerp(velocity.z, direction.z * speed, ACCELERATION * delta)
+			velocity.x = lerp(velocity.x, direction.x * max_speed, ACCELERATION * delta)
+			velocity.z =  lerp(velocity.z, direction.z * max_speed, ACCELERATION * delta)
 		else:
-			velocity.x = lerp(velocity.x, 0.0, FRICTION * delta)
-			velocity.z = lerp(velocity.z, 0.0, FRICTION * delta)
+			velocity.x = lerp(velocity.x, 0.0, deceleration * delta)
+			velocity.z = lerp(velocity.z, 0.0, deceleration * delta)
 	
 	if Input.is_action_just_pressed("dash") and can_dash:
 		dash()
@@ -81,7 +86,7 @@ func jump():
 	if is_on_floor():
 		velocity.y = DEFAULT_JUMP_VELOCITY
 	elif is_wall_sliding:
-		var dir = get_wall_normal()
+		var dir = get_wall_normal() * 0.5
 		dir.y = WALL_JUMP_Y_DIRECTION
 		velocity = dir * WALL_JUMP_VELOCITY
 	

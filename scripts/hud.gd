bf555dcec95216328b8387f77f20c7faed0d106a
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2025-02-18 17:44:41 -0800

    remove random prints

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,6 +5,4 @@ class_name HUD extends Node
 @onready var dash_bar: ProgressBar = $PlayerStatus/AspectRatioContainer/BarVBoxContainer/DashBar
 
 func update_ammo_display(num_bullets, mag_capacity):
-	print(num_bullets, mag_capacity)
-	print((num_bullets / mag_capacity) * 100)
 	ammo_amount_bar.value = (float(num_bullets) / mag_capacity) * 100

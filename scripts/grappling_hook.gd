db1bc85c4e3afadd20b3c7759a9b41012b22dd9f
daylin <tootymcfruity8@gmail.com>
2025-02-19 10:55:18 -0800

    added placeholders for sfx and stuff

--- scripts/grappling_hook.gd
+++ scripts/grappling_hook.gd
@@ -3,12 +3,16 @@ class_name GrapplingHook extends Node
 @onready var player: CharacterBody3D 
 @onready var grapple_ray: RayCast3D = $GrappleRay
 
-const REST_LENGTH: float = 2.0
-const STIFFNESS: float = 10.0
-var damping: float = 1.0
+@export var range: float = 50.0 
+@export var rest_length: float = 2.0
+@export var stiffness: float = 10.0
+@export var damping: float = 1.0
 
 var target: Vector3
 var is_launched: bool = false
+
+func _ready() -> void:
+	grapple_ray.target_position = Vector3(0, 0, -range)
 
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
@@ -18,25 +22,29 @@ func _physics_process(delta: float) -> void:
 	
 	if is_launched:
 		handle_grapple(delta)
+		if not $GrappleSoundEffect.playing:
+			$GrappleSoundEffect.play()
 
 func launch():
+	$LaunchSoundEffect.play()
 	if grapple_ray.is_colliding():
 		target = grapple_ray.get_collision_point()
 		is_launched = true
 		
 func retract():
+	$RetractSoundEffect.play()
 	is_launched = false
 		
 func handle_grapple(delta):
 	var target_direction = player.global_position.direction_to(target)
 	var target_distance = player.global_position.distance_to(target)
 	
-	var displacement = target_distance - REST_LENGTH
+	var displacement = target_distance - rest_length
 	
 	var force = Vector3.ZERO
 	
 	if displacement > 0:
-		var spring_force = STIFFNESS * displacement * target_direction
+		var spring_force = stiffness * displacement * target_direction
 		
 		var vel_dot = player.velocity.dot(target_direction)
 		var damping = -damping * vel_dot * target_direction

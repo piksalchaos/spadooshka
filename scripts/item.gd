ffb21254453cce88e86261c9a734b7dffd0b695a
daylin <tootymcfruity8@gmail.com>
2025-02-19 13:50:53 -0800

    Update item.gd

--- scripts/item.gd
+++ scripts/item.gd
@@ -3,6 +3,7 @@ class_name Item extends Node3D
 @onready var player: CharacterBody3D
 @export var need_camera: bool
 @export var item_name: String
+@export var hud_icon: Texture2D
 
 func use() -> bool:
 	return true

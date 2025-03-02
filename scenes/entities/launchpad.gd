5505ee1f0b3e4fee7af467a23aa38532fd7f3fed
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2025-03-02 15:34:55 -0800

    launchpad

--- scenes/entities/launchpad.gd
+++ scenes/entities/launchpad.gd
@@ -1,5 +1,6 @@
-extends Node3D
+class_name Launchpad extends MeshInstance3D
 
+@export var strength = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is StaticBody3D:
+		return
+	if body is RigidBody3D:
+		body.apply_impulse(strength * basis.y.normalized())
+		return
+	body.velocity += strength * basis.y.normalized()

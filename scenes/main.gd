f524c2a27cab60542ba19023b364be9479d2dc7f
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-03-23 12:50:54 -0700

    created gui.gd to separate functionality from main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -19,8 +19,8 @@ var enet_peer = ENetMultiplayerPeer.new()
 var failed_listen_port_bind = false
 var server_browser: ServerBrowser
 
-@onready var title_screen: Control = $GUI/TitleScreen
-@onready var main_menu: Control = $GUI/MainMenu
+@onready var gui: Control = $GUI
+
 @onready var multiplayer_container: Node = $MultiplayerContainer
 @onready var local_menu: LocalMenu = $GUI/LocalMenu
 @onready var lobby_menu: LobbyMenu = $GUI/LobbyMenu
@@ -39,16 +39,10 @@ signal score_changed(round_number: int, P1_score: int, P2_score: int)
 func _ready() -> void:
 	score_changed.connect(hud.update_score_display.rpc)
 
-func _on_title_screen_start_button_pressed() -> void:
-	title_screen.hide()
-	main_menu.show()
-
 func _on_title_screen_exit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_main_menu_local_game_button_pressed() -> void:
-	main_menu.hide()
-	local_menu.show()
 	set_up_server_browser()
 
 func _on_local_menu_host_button_pressed() -> void:
@@ -57,8 +51,6 @@ func _on_local_menu_host_button_pressed() -> void:
 	
 	multiplayer.peer_connected.connect(lobby_menu.on_peer_connected)
 	multiplayer.peer_disconnected.connect(lobby_menu.on_peer_disconnected)
-	lobby_menu.add_player_display(multiplayer.get_unique_id())
-	lobby_menu.show()
 	
 	if server_browser:
 		server_browser.set_up_broadcast("placeholder username")
@@ -77,23 +69,9 @@ func _on_local_menu_join_button_pressed(ip_address: String) -> void:
 func join_by_ip(ip_address: String):
 	enet_peer.create_client(ip_address, server_port)
 	multiplayer.multiplayer_peer = enet_peer
-	lobby_menu.show()
 
 func _on_local_menu_singleplayer_button_pressed() -> void:
 	play_game()
-
-func _on_lobby_menu_ready_button_pressed(peer_id: int, is_ready: bool) -> void:
-	update_peer_ready_states.rpc(peer_id, is_ready)
-
-@rpc("any_peer", "call_local")
-func update_peer_ready_states(peer_id, is_peer_ready):
-	peer_ready_states[peer_id] = is_peer_ready
-	var peer_ready_count = peer_ready_states.values().count(true)
-	var are_peers_ready = peer_ready_count == multiplayer.get_peers().size()+1
-	if multiplayer.get_unique_id() == 1:
-		lobby_menu.set_start_button_visibility(are_peers_ready)
-	else:
-		lobby_menu.set_waiting_label_visibility(are_peers_ready)
 
 func _on_lobby_menu_start_button_pressed() -> void:
 	assert(multiplayer.get_unique_id() == 1, \
@@ -101,17 +79,16 @@ func _on_lobby_menu_start_button_pressed() -> void:
 	play_game()
 
 func play_game():
-	prepare_client_for_game.rpc()
+	gui.prepare_for_game.rpc()
+	close_server_browser.rpc()
 	choose_map()
 	add_players()
 	play_round()
 
 @rpc("call_local")
-func prepare_client_for_game():
+func close_server_browser():
 	if server_browser:
 		server_browser.queue_free()
-	lobby_menu.hide()
-	hud.show()
 
 func choose_map():
 	map = load(MAP_FILE_NAMES.pick_random()).instantiate()
@@ -143,7 +120,6 @@ func play_round():
 @rpc("any_peer", "call_local")
 func end_round(dead_peer_id: int):
 	if not is_multiplayer_authority(): return
-	
 	if dead_peer_id != 1:
 		rounds_won += 1
 	var P1_score: int = rounds_won
@@ -154,15 +130,7 @@ func end_round(dead_peer_id: int):
 		score_changed.emit(round_number, P1_score, P2_score)
 		play_round()
 	else:
-		prepare_GUI_for_end_game.rpc(P1_score == ROUNDS_TO_WIN)
-
-@rpc("call_local")
-func prepare_GUI_for_end_game(P1_won: bool):
-	hud.hide()
-	if is_multiplayer_authority() == P1_won:
-		$GUI/VictoryScreen.show()
-	else:
-		$GUI/DefeatScreen.show()
+		gui.prepare_for_end_game.rpc(P1_score == ROUNDS_TO_WIN)
 
 func _on_multiplayer_container_child_entered_tree(node: Node) -> void:
 	if node is Player:

5799d6eccabe47918f957261e6acf44f14cb2d79
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-03-27 20:16:57 -0700

    bringing back randomized items + minimizer item in lootbox

--- scenes/entities/loot_box.gd
+++ scenes/entities/loot_box.gd
@@ -5,7 +5,8 @@ const ITEM_FILE_NAMES: PackedStringArray = [
 	"res://scenes/items/jump_boost_item.tscn",
 	"res://scenes/items/grappling_hook_item.tscn",
 	"res://scenes/items/grenade_item.tscn",
-	"res://scenes/items/health_burger_item.tscn"
+	"res://scenes/items/health_burger_item.tscn",
+	"res://scenes/items/minimizer_item.tscn"
 ]
 
 @export var item: Item
@@ -13,8 +14,7 @@ const ITEM_FILE_NAMES: PackedStringArray = [
 
 func _ready() -> void:
 	var random_index: int = randi_range(0, ITEM_FILE_NAMES.size() - 1)
-	#item = load(ITEM_FILE_NAMES[random_index]).instantiate()
-	item = load("res://scenes/items/speed_boost_item.tscn").instantiate()
+	item = load(ITEM_FILE_NAMES[random_index]).instantiate()
 
 func obtain_item() -> Item:
 	queue_free_for_all_peers.rpc()

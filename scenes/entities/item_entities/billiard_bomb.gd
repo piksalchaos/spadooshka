e183656595356d13b9ba91a9ad47db09f4ad7b26
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-03-30 04:27:16 -0700

    done with billiard balls at last!!!

--- scenes/entities/item_entities/billiard_bomb.gd
+++ scenes/entities/item_entities/billiard_bomb.gd
@@ -1,34 +1,27 @@
 class_name BilliardBomb extends RigidBody3D
 
-const BILLIARD_BALL = preload("res://scenes/entities/item_entities/billiard_ball.tscn")
 const NUMBER_OF_BALLS = 15
 const BALL_SPEED = 18.0
 const BALL_DISTANCE_FROM_ORIGIN = 1.0
+const BALL_RANGE = PI/3
 
 var enabled = false
 
 func _on_enable_timer_timeout() -> void:
 	enabled = true
-	#if get_contact_count() > 0:
-		#explode()
-	
-#func _on_body_entered(_body: Node) -> void:
-	#if enabled:
-		#explode()
 
 func explode(vector_direction: Vector3):
 	for i in NUMBER_OF_BALLS:
-		#var vector_direction = get_random_upward_unit_vector()
-		var offset_vector_direction = get_randomly_offset_vector(vector_direction, PI/3)
-		print("offset vector dir: ", offset_vector_direction)
+		var offset_vector_direction = get_randomly_offset_vector(vector_direction, BALL_RANGE)
 		SpawnerManager.spawn_with_data({
 			"path": "res://scenes/entities/item_entities/billiard_ball.tscn",
 			"props": {
 				"position": position + offset_vector_direction * BALL_DISTANCE_FROM_ORIGIN,
-				"linear_velocity": offset_vector_direction * BALL_SPEED
+				"linear_velocity": offset_vector_direction * BALL_SPEED,
+				"rotation": Vector3(randf()*2*PI, randf()*2*PI, randf()*2*PI),
+				"texture_index": i
 			}
 		})
-		
 	queue_free()
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
@@ -36,7 +29,6 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	for i in range(state.get_contact_count()):
 		var normal = state.get_contact_local_normal(i)
 		explode(normal)
-		print("Collision normal at contact ", i, ": ", normal)
 
 func get_randomly_offset_vector(vector: Vector3, max_angle: float) -> Vector3:
 	var rotation_x = get_randomly_rotated_quat(Vector3.RIGHT, max_angle)
